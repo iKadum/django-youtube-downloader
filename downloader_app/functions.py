aef20e44592371768f1f45966bbf210ec242de70
Igor Kadum <igor.kadum@gmail.com>
2025-11-27 20:01:41 +0100

    add pytubefix library

--- downloader_app/functions.py
+++ downloader_app/functions.py
@@ -1,2 +1,9 @@
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+
+
 def download(url):
-    print(f"download: {url}")
+    yt = YouTube(url, on_progress_callback=on_progress)
+    ys = yt.streams.get_highest_resolution()
+    ys.download()
+    print(f"Video '{yt.title}' successfully downloaded")
